d04bb323acb90ffd9faff43ea0a7f3932b3a7d8d
aidanc2004 <67815343+aidanc2004@users.noreply.github.com>
2024-06-02 02:54:05 -0300

    Add GitHub-like language tags

--- src/script.js
+++ src/script.js
@@ -16,7 +16,7 @@ const closeDescription = document.getElementById("closeDescription");
 
 closeDescription.addEventListener("click", toggleDescription)
 
-// Transition on loading website
+// Header ocpacity transitions on loading website
 headerImage.style.opacity = 1;
 headerName.style.opacity = 1;
 headerScroll.style.opacity = 1;
@@ -35,6 +35,7 @@ let projects = [
         "shortDesc": "Very simple shell written in C.",
         "description": `A very simple shell I wrote to get more familiar with C and low level programming.`,
         "link": `${github}shell`,
+        "lang": "C"
     },
     {
         "name": "Game of Life",
@@ -44,7 +45,8 @@ let projects = [
             die if they have >3 or <2 neighbours, and a dead cell becomes alive with 3 neighbours. It's in C with the Raylib graphics library I wrote while learning
             cellular automata. It takes the number of rows and columns as command line arguments and clicking will add or remove a cell.`,
         "link": `${github}game-of-life`,
-        "img": "Game-Of-Life.gif"
+        "img": "Game-Of-Life.gif",
+        "lang": "C"
     },
     {
         "name": "acorn-downloader",
@@ -54,7 +56,8 @@ let projects = [
             that I've missed or haven't downloaded. To make this easier, I made a web scraper using Perl with WWW::Mechanize to automatically login and download
             all resources from all of my in-progress courses into seperate directories.`,
         "link": `${github}acorn-downloader`,
-        "img": "Acorn-Downloader.png"
+        "img": "Acorn-Downloader.png",
+        "lang": "Perl"
     },
     {
         "name": "wheelock-perl",
@@ -62,14 +65,16 @@ let projects = [
         "description": `I spend a lot of time working in the terminal, so I decided to quickly reimplement my Swift frontend for Acadia University's dining hall as
         a command line app using Perl. You can choose a period to view (breakfast, lunch, dinner), view the menu at a specific date, and filter by categories.`,
         "link": `${github}wheelock-perl`,
-        "img": "Wheelock-Perl.png"
+        "img": "Wheelock-Perl.png",
+        "lang": "Perl"
     },
     {
         "name": "Campground Reservation Website",
         "shortDesc": "A Ruby on Rails web application where customers can book reservations for a campsite.",
         "description": "While working at Jeff's Hideaway campground as a summer job, I worked a lot with their campground reservation software,\n        Campground Master, and their website made using Wordpress. I noticed how these two programs worked with different databases, meaning that once\n        someone booked a site we would have to copy it over to the other program. This inspired me to create my own reservation software.",
         "link": `${github}Campground-Website`,
-        "img": "Campground.png"
+        "img": "Campground.png",
+        "lang": "Ruby"
     },
     {
         "name": "Wheelock Hall",
@@ -78,33 +83,37 @@ let projects = [
         "description": "Being a student at Acadia, I use the online menu at Dine on Campus all the time to figure out what's on the menu,\n        but I found it annoying to constantly need go to the website to check so I decided to make my own frontend. The app was\n        also designed to work with any other school that uses Dine on Campus for it's menu with minimal tweaking.",
         "link": `${github}Wheelock-Hall-App`,
         "img": "Wheelock-1.png",
-        "img2": "Wheelock-2.png"
+        "img2": "Wheelock-2.png",
+        "lang": "Swift"
     },
     {
         "name": "Graphing Calculator",
         "shortDesc": "A graphing calculator made with JavaScript and HTML canvas.",
         "description": "A graphing calculator made with HTML, CSS and vanilla Javascript that can graph multiple different equations at once\n        and show a clicked point on a function. You can manipulate the shown domain and range by dragging and scrolling the graph. There are\n        also example equations in the help menu in the bottom left.",
         "link": "/src/graphing-calculator",
+        "linkName": "Page",
         "img": "Graphing-Calculator.png",
-        "linkName": "Page"
+        "lang": "JS"
     },
     {
         "name": "NodeJS Chatroom",
         "shortDesc": "A Discord-like chatroom written in JavaScript and NodeJS using WebSockets.",
         "description": "A very simple proof of concept chatroom web app inspired by Discord, with the frontend in vanilla Javascript, HTML, and CSS and\n        a backend server in NodeJS where multiple users and join and chat with each other. Any user that joins can see the chat history of the server,\n        and can change their profile picture and username color, which will be reflected on the other users.",
         "link": `${github}chatroom`,
-        "img": "Chatroom.png"
+        "img": "Chatroom.png",
+        "lang": "JS"
     },
     {
         "name": "C++ Sorting Visualizations",
         "shortDesc": "A sorting algorithm visualizations written in C++ with SFML.",
         "description": "A C++ and SFML program which showcases different sorting algorithms and the amount of steps it takes to do complete them.\n        This includes alogrithms such as selection sort, insertion sort, quick sort, and also more impractical algorithms such as\n        bogo sort and sleep sort.",
         "link": "https://github.com/aidanc2004/Cpp-Sorting-Visualization",
-        "img": "Sorting.png"
+        "img": "Sorting.png",
+        "lang": "C++"
     }
 ];
 
-// Generate projects from array of objects
+// Generate project thumbnails from array of objects
 for (let i = 0; i < projects.length; i++) {
     let project = projects[i];
 
@@ -112,24 +121,45 @@ for (let i = 0; i < projects.length; i++) {
     projectDiv.className = `border-2 bg-dark-gray border-gray rounded-lg m-2 p-4 hover:drop-shadow-[0_0_0.5rem_#fa508c]
                             cursor-pointer transition-[drop_shadow] duration-150 ease-in-out`;
 
+    // Project Title
     let projectName = document.createElement("h2");
     projectName.textContent = project.name;
     projectName.className = "text-purple";
 
+    // Thumbnail description
     let projectShortDesc = document.createElement("p");
     projectShortDesc.textContent = project.shortDesc;
 
-    let projectThumb = document.createElement("img");
-
+    // If it doesn't have a thumbnail then use the regular image
     if (project.thumb == undefined) project.thumb = project.img;
 
+    let projectThumb = document.createElement("img");
     projectThumb.src = "/src/imgs/" + project.thumb;
     projectThumb.alt = project.name;
     projectThumb.className = "pt-4";
 
+    // Language tag
+    let language = project.lang;
+    let languageLower = language.toLowerCase();
+    if (language == "C++") languageLower = "cpp";
+
+    let languageTagDiv = document.createElement("div");
+    languageTagDiv.className = `flex w-fit mt-2 px-2 items-center rounded-full border-2 ${languageLower}-border`;
+
+    let languageTagDot = document.createElement("div");
+    languageTagDot.className = `h-2 w-2 ${languageLower}-bg rounded-full mr-1 inline-block`;
+
+    let languageTagName = document.createElement("p");
+    languageTagName.className = "text-sm";
+    languageTagName.textContent = project.lang;
+
+    languageTagDiv.appendChild(languageTagDot);
+    languageTagDiv.appendChild(languageTagName);
+
     projectDiv.appendChild(projectName);
     projectDiv.appendChild(projectShortDesc);
     projectDiv.appendChild(projectThumb);
+    projectDiv.appendChild(languageTagDiv)
 
     projectsGrid.appendChild(projectDiv);
 }
